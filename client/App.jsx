f4364d9ba1677449e0caff721bb4a6b3a4072579
Oscar Korz <okorz001@gmail.com>
2019-04-22 19:10:51 -0700

    Router hello world

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+function LanguageSelectPage() {
+    return <div>Select your language.</div>
+}
+
+function HomePage({match}) {
+    const {learning, from} = match.params
+    return <div>You're learning {learning} from {from}</div>
+}
 
 export default function App() {
-    return <div>Hello world!</div>
+    return (
+        <Router>
+            <div>
+                <Route path="/" exact component={LanguageSelectPage} />
+                <Route path="/:learning/:from" component={HomePage} />
+            </div>
+        </Router>
+    )
 }
