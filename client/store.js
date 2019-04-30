2964ea6f5e9f2fdc0f71fe23fe1a62968ce5dfc9
Oscar Korz <okorz001@gmail.com>
2019-04-29 21:33:52 -0700

    Add redux-logger

--- client/store.js
+++ client/store.js
@@ -1,4 +1,5 @@
 import {applyMiddleware, createStore} from 'redux'
+import {createLogger} from 'redux-logger'
 import {router5Middleware} from 'redux-router5'
 
 import reducer from './reducers'
@@ -6,6 +7,10 @@ import reducer from './reducers'
 export default function newStore(router, initialState) {
     const mw = []
     mw.push(router5Middleware(router))
-    const createStoreWithMw = applyMiddleware(...mw)(createStore)
-    return createStoreWithMw(reducer, initialState)
+
+    // debugging tools last to ensure we have regular actions
+    const logger = createLogger()
+    mw.push(logger)
+
+    return createStore(reducer, initialState, applyMiddleware(...mw))
 }
