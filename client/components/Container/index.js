cd3bfd9548e945f8015a85c54195dc2a7b862c77
Oscar Korz <okorz001@gmail.com>
2019-05-07 21:49:01 -0700

    Disable DevTools for now
    react-json-tree is crashing which unmounts the entire app.

--- client/components/Container/index.js
+++ client/components/Container/index.js
@@ -3,5 +3,7 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./Container.prod')
 }
 else {
-    module.exports = require('./Container.dev')
+    // TODO: react-json-tree is crashing: Cannot read property 'length' of null
+    //module.exports = require('./Container.dev')
+    module.exports = require('./Container.prod')
 }
