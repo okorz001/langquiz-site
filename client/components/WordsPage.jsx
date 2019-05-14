39ac4265ce3d1e216ee6cf7e52b5572815417e68
Oscar Korz <okorz001@gmail.com>
2019-05-14 09:32:16 -0700

    table pagination

--- client/components/WordsPage.jsx
+++ client/components/WordsPage.jsx
@@ -5,7 +5,9 @@ import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
+import TableFooter from '@material-ui/core/TableFooter'
 import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
 import TableRow from '@material-ui/core/TableRow'
 import {withStyles} from '@material-ui/core/styles'
 
@@ -18,45 +20,82 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
     },
+    pagination: {
+        display: 'flex',
+        flexFlow: 'row wrap',
+    },
 }
 
-export function WordsPage({classes, words}) {
-    console.log(words[0])
-    const rows = words.filter(words => words.translations.length)
-    .map(word => {
-        const translations = word.translations.map(it => (
-            <TableCell key={it}>{it}</TableCell>
-        ))
-        return (
-        <React.Fragment>
-            <TableRow key={word.word}>
-                <TableCell rowSpan={word.translations.length}>
-                    {word.word}
-                </TableCell>
-                {translations[0]}
-            </TableRow>
-            {translations.slice(1).map(it => <TableRow>{it}</TableRow>)}
-        </React.Fragment>
-        )
-    })
-    return (
-        <div className={classes.root}>
-            <RequireCourse />
-            <Paper>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Word</TableCell>
-                            <TableCell>Translations</TableCell>
+export class WordsPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            page: 0,
+            rowsPerPage: 10,
+        }
+
+        this.setPage = (event, page) => {
+            this.setState({page})
+        }
+
+        this.setRowsPerPage = (event) => {
+            this.setState({rowsPerPage: event.target.value})
+        }
+    }
+
+    render() {
+        const {classes, words} = this.props
+        const {page, rowsPerPage} = this.state
+
+        const rows = words
+            // TODO: some words have zero translations?!
+            .filter(words => words.translations.length)
+            .slice((page) * rowsPerPage, (page + 1) * rowsPerPage)
+            .map(word => {
+                const translations = word.translations.map(it => (
+                    <TableCell key={it}>{it}</TableCell>
+                ))
+                return (
+                    <React.Fragment>
+                        <TableRow key={word.word}>
+                            <TableCell rowSpan={word.translations.length}>
+                                {word.word}
+                            </TableCell>
+                            {translations[0]}
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows}
-                    </TableBody>
-                </Table>
-            </Paper>
-        </div>
-    )
+                        {translations.slice(1).map(it => <TableRow>{it}</TableRow>)}
+                    </React.Fragment>
+                )
+            })
+
+        return (
+            <div className={classes.root}>
+                <RequireCourse />
+                <Paper>
+                    <Table padding="dense">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Word</TableCell>
+                                <TableCell>Translations</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows}
+                        </TableBody>
+                        <TableFooter>
+                            <TablePagination count={words.length}
+                                             page={page}
+                                             onChangePage={this.setPage}
+                                             rowsPerPage={rowsPerPage}
+                                             onChangeRowsPerPage={this.setRowsPerPage}
+                                             />
+                        </TableFooter>
+                    </Table>
+                </Paper>
+            </div>
+        )
+    }
 }
 
 export default compose(
