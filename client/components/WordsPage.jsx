b4dfdaca540a81c4f698de9cd0cebc5f0b519c91
Oscar Korz <okorz001@gmail.com>
2019-05-13 23:03:35 -0700

    table

--- client/components/WordsPage.jsx
+++ client/components/WordsPage.jsx
@@ -1,23 +1,69 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import Typography from '@material-ui/core/Typography'
+import {compose} from 'redux'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import {withStyles} from '@material-ui/core/styles'
 
 import RequireCourse from './RequireCourse'
 import {getCurrentCourseWords} from '../selectors'
 
-export function WordsPage({words}) {
-    return (
+const styles = {
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+}
+
+export function WordsPage({classes, words}) {
+    console.log(words[0])
+    const rows = words.filter(words => words.translations.length)
+    .map(word => {
+        const translations = word.translations.map(it => (
+            <TableCell key={it}>{it}</TableCell>
+        ))
+        return (
         <React.Fragment>
-            <RequireCourse />
-            <Typography paragraph>
-                Found {words.length} words.
-            </Typography>
+            <TableRow key={word.word}>
+                <TableCell rowSpan={word.translations.length}>
+                    {word.word}
+                </TableCell>
+                {translations[0]}
+            </TableRow>
+            {translations.slice(1).map(it => <TableRow>{it}</TableRow>)}
         </React.Fragment>
+        )
+    })
+    return (
+        <div className={classes.root}>
+            <RequireCourse />
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Word</TableCell>
+                            <TableCell>Translations</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </div>
     )
 }
 
-export default connect(
-    state => ({
-        words: getCurrentCourseWords(state),
-    })
+export default compose(
+    connect(
+        state => ({
+            words: getCurrentCourseWords(state),
+        })
+    ),
+    withStyles(styles)
 )(WordsPage)
