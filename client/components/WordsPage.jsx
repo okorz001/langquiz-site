2b933926371844e5637430879a5396e0045600e3
Oscar Korz <okorz001@gmail.com>
2019-05-14 18:07:40 -0700

    search

--- client/components/WordsPage.jsx
+++ client/components/WordsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {compose} from 'redux'
+import InputBase from '@material-ui/core/InputBase'
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -10,47 +11,103 @@ import TableHead from '@material-ui/core/TableHead'
 import TablePagination from '@material-ui/core/TablePagination'
 import TableRow from '@material-ui/core/TableRow'
 import {withStyles} from '@material-ui/core/styles'
+import {fade} from '@material-ui/core/styles/colorManipulator'
+import SearchIcon from '@material-ui/icons/SearchOutlined'
 
 import RequireCourse from './RequireCourse'
 import {getCurrentCourseWords} from '../selectors'
 
-const styles = {
+const styles = theme => ({
     root: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
+
+
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    width: '100%',
+  },
+
     pagination: {
         display: 'flex',
         flexFlow: 'row wrap',
     },
-}
+})
 
 export class WordsPage extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            search: null,
             page: 0,
             rowsPerPage: 10,
+        }
+
+        this.setSearch = event => {
+            this.setState({
+                search: event.target.value,
+                // reset pagination on search change
+                page: 0,
+            })
         }
 
         this.setPage = (event, page) => {
             this.setState({page})
         }
 
-        this.setRowsPerPage = (event) => {
+        this.setRowsPerPage = event => {
             this.setState({rowsPerPage: event.target.value})
         }
     }
 
     render() {
         const {classes, words} = this.props
-        const {page, rowsPerPage} = this.state
+        const {search, page, rowsPerPage} = this.state
 
-        const rows = words
+        const selectedWords = words
             // TODO: some words have zero translations?!
-            .filter(words => words.translations.length)
+            .filter(word => word.translations.length)
+            .filter(word => {
+                if (!search) {
+                    return true
+                }
+                return !search ||
+                    // TODO: locale-safe and caps insensitive
+                    word.word.search(search) != -1 ||
+                    word.translations.some(it => it.search(search) != -1)
+            })
+
+        const rows = selectedWords
             .slice((page) * rowsPerPage, (page + 1) * rowsPerPage)
             .map(word => {
                 const translations = word.translations.map(it => (
@@ -73,6 +130,17 @@ export class WordsPage extends React.Component {
             <div className={classes.root}>
                 <RequireCourse />
                 <Paper>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase classes={{
+                                       root: classes.inputRoot,
+                                       input: classes.inputInput
+                                   }}
+                                   placeholder="Search..."
+                                   onChange={this.setSearch} />
+                    </div>
                     <Table padding="dense">
                         <TableHead>
                             <TableRow>
@@ -84,7 +152,7 @@ export class WordsPage extends React.Component {
                             {rows}
                         </TableBody>
                         <TableFooter>
-                            <TablePagination count={words.length}
+                            <TablePagination count={selectedWords.length}
                                              page={page}
                                              onChangePage={this.setPage}
                                              rowsPerPage={rowsPerPage}
